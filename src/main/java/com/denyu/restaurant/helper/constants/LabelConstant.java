5fc52d1a36cca8a4785306f58c587f2428f2522c
dendy192 <dtiawan192@gmail.com>
2024-06-08 14:32:36 +0700

    add label constant

---
+++ src/main/java/com/denyu/restaurant/helper/constants/LabelConstant.java
@@ -0,0 +1,9 @@
+package com.denyu.restaurant.helper.constants;
+
+public class LabelConstant {
+
+    public static String messages = "Messages";
+    public static String code = "code";
+    public static String meta = "meta";
+    public static String data = "data";
+}
