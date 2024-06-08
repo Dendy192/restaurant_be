4c058f6d7b664f7214ea95c5dbdfbe979d69438f
dendy192 <dtiawan192@gmail.com>
2024-06-08 17:03:53 +0700

    modify LabelConstant

--- src/main/java/com/denyu/restaurant/constant/ProductConstant.java
+++
@@ -1,6 +0,0 @@
-package com.denyu.restaurant.constant;
-
-public class ProductConstant {
-
-    public static String productNotFound = "Product Not Found";
-}
--- src/main/java/com/denyu/restaurant/helper/constants/LabelConstant.java
+++ src/main/java/com/denyu/restaurant/helper/constants/LabelConstant.java
@@ -1,9 +1,20 @@
 package com.denyu.restaurant.helper.constants;
 
 public class LabelConstant {
+    public static String id = "id";
+    //product
+    public static String productNotFound = "Product Not Found";
 
+    //user
+    public static String userNotFound = "User Not Found";
+    public static String userFound ="User Found";
+    public static String name = "name";
+    public static String role = "role";
+
+    //response
     public static String messages = "Messages";
     public static String code = "code";
     public static String meta = "meta";
     public static String data = "data";
+    public static String result = "result";
 }
